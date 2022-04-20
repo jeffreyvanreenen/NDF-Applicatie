99b1cf0766bee0181892714dc45faaf7b4fa7139
Jeffrey van Reenen <jeffrey92.hrb@gmail.com>
2022-04-20 19:17:35 +0200

    initial commit

--- resources/views/search/searchtermindex.blade.php
+++ resources/views/search/searchtermindex.blade.php
@@ -38,7 +38,7 @@
                 </div>
                 <div class="mb-4 w-3/6">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="telegram_chat_id">
-                        Telegram Chat ID (leeglaten is disabled)
+                        Telegram Chat ID (leeglaten is disabled.)
                     </label>
                     <input
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
